e7ae13b57c89a9bf4d0d1ebb4905962bdfda6086
garrentmh <garrentmh@gmail.com>
2025-07-04 15:28:16 +0800

    derived part

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,12 +7,16 @@
     function onclick() {
         status = status === 'OPEN' ? 'CLOSED' : 'OPEN';
     }
+    let full_name = $derived(name + ' ' + 'Harrison');
+
     // function toggle() {
     //     status = status === 'OPEN' ? 'CLOSED' : 'OPEN';
     // }
 </script>
 
 <Header {name} />
+
+<h2>{full_name}</h2>
 
 <input type="text" bind:value={name}/>
 
